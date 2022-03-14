ac5ea8606e0e3eeb9c40c70c9115dd733eb7ced4
[fredok]gitconfig --global user.email [fredericksonde@gmail.com] <frederick.sonder@laplateforme.io>
2022-03-14 11:43:36 +0100

    ajout job05

---
+++ job05/script.js
@@ -0,0 +1,9 @@
+function afficherjourssemaines()
+{
+    var jourssemaines = ['lundi', 'mardi', 'mercredi', 'jeudi','vendredi','samedi', 'dimanche']
+    for (var jours of jourssemaines)
+    console.log(jours)
+        
+}
+
+afficherjourssemaines()
