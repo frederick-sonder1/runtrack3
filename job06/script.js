aac013ddc15747253fc35a69e4cfce73fc40b2b6
[fredok]gitconfig --global user.email [fredericksonde@gmail.com] <frederick.sonder@laplateforme.io>
2022-03-14 12:07:28 +0100

    ajout job6

---
+++ job06/script.js
@@ -0,0 +1,20 @@
+function fizzbuzz()
+{
+    for (var i = 1; i <= 151; i++)
+    { 
+
+        if (i%3==0 && i%5==0){
+            console.log("fizzbuzz")
+        }
+        else if (i%5==0){
+            console.log("buzz")
+        }
+        else if (i%3==0){
+            console.log("fizz")
+        }
+        else{
+            console.log(i)
+        }
+    }
+}
+fizzbuzz();
