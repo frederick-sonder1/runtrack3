20c5899126a94cc819a44bca08d342befa51ad6e
[fredok]gitconfig --global user.email [fredericksonde@gmail.com] <frederick.sonder@laplateforme.io>
2022-03-15 10:30:50 +0100

    job 8

--- jour01/job08/script.js
+++ jour01/job08/script.js
@@ -1,14 +1,24 @@
-function sommenombrepremier(var1, var2)
+function sommenombrepremier(var1, var2) 
 {
-    if ((var1 % 1 === 0) && (var1 % var1 === 0))
-    {
-        if ((var2 % 1 === 0) && (var2 % var2 === 0))
-        {
-            if (var1 % var2 === 0)
-            {
-                console.log(var1+var2)
+    
+    estPremier = true;
+
+    // 1 ne compte pas comme un nombre premier
+    if(var1 > 1 && var2 > 1) {
+        for(i = 2; i < var1, i < var2; i++) {
+            if(var1 % i == 0 && var2 % i == 0) {
+                estPremier = false;
+                break;
             }
         }
-    } 
+
+        if(estPremier) {
+            return var1 + var2;
+        }
+        else {
+            console.log("L'un des nombres n'est pas premier.");
+            return false;
+        }
+    }
 }
-sommenombrepremier(3,3)
+console.log(sommenombrepremier(4, 7));
